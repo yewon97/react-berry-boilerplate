beb38622c31142eca45f7f760aac86fedd2b0675
yewon97 <dpdnjs9936@gmail.com>
2024-01-08 23:15:58 +0900

    firebase 환경변수 등록하기

--- src/remote/firebase.ts
+++ src/remote/firebase.ts
@@ -1,14 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 
+const {
+  REACT_APP_API_KEY,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_PROJECT_ID,
+  REACT_APP_STORAGE_BUCKET,
+  REACT_APP_MESSAGEING_SENDER_ID,
+  REACT_APP_APP_ID,
+  REACT_APP_MEASUREMENT_ID,
+} = process.env
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyADx3tRBC-pXuM1Cz5ZJcFoaUg-kA7VI3o',
-  authDomain: 'fastcampus-mcard-c6d87.firebaseapp.com',
-  projectId: 'fastcampus-mcard-c6d87',
-  storageBucket: 'fastcampus-mcard-c6d87.appspot.com',
-  messagingSenderId: '698797828044',
-  appId: '1:698797828044:web:32d94fb1840bdbdf031f69',
-  measurementId: 'G-Y807YXKV26',
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGEING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
+  measurementId: REACT_APP_MEASUREMENT_ID,
 }
 
-const app = initializeApp(firebaseConfig)
+export const app = initializeApp(firebaseConfig)
