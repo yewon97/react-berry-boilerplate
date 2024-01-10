03fa8ce78194efb9544bc3b891b96fe1a60aedac
yewon97 <dpdnjs9936@gmail.com>
2024-01-11 00:13:45 +0900

    Update: 에러땜에 config파일 다 건들여봄

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,7 @@ import Text from '@shared/Text'
 function App() {
   return (
     <div>
-      <Text typography="t1">t1</Text>
-      <Text typography="t2">t2</Text>
-      <Text typography="t3">t3</Text>
-      <Text typography="t4">t4</Text>
-      <Text typography="t5">t5</Text>
-      <Text typography="t6">t6</Text>
-      <Text>t7</Text>
+      <Text typography="t1">hi</Text>
     </div>
   )
 }
