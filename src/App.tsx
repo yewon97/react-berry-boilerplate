843ee932eed160fb1753bdc6ec800f68508e1ece
yewon97 <dpdnjs9936@gmail.com>
2024-01-12 00:52:07 +0900

    Text 컴포넌트 테스트해보기

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,15 @@ import Text from '@shared/Text'
 function App() {
   return (
     <div>
-      <Text typography="t1">hi</Text>
+      <Text typography="t1" display={'block'}>
+        hi
+      </Text>
+      <Text typography="t2">hi</Text>
+      <Text typography="t3">hi</Text>
+      <Text typography="t4">hi</Text>
+      <Text typography="t5" color="red">
+        hi
+      </Text>
     </div>
   )
 }
