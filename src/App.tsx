693d7392012dd8668743670f6447195767582de1
yewon97 <dpdnjs9936@gmail.com>
2024-01-14 21:19:45 +0900

    button 공통 컴포넌트 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Text from '@shared/Text'
+import Button from '@shared/Button'
 
 function App() {
   return (
@@ -13,6 +14,26 @@ function App() {
       <Text typography="t5" color="red">
         hi
       </Text>
+
+      <div style={{ height: 10, width: '100%', background: '#efefef' }}>
+        <Button>클릭해주세요</Button>
+        <Button color="primary">클릭해주세요</Button>
+        <Button color="success">클릭해주세요</Button>
+        <Button color="error">클릭해주세요</Button>
+        <Button weak={true} color="primary">
+          클릭해주세요
+        </Button>
+        <Button weak={true} color="success">
+          클릭해주세요
+        </Button>
+        <Button weak={true} color="error">
+          클릭해주세요
+        </Button>
+        <Button full={true}>클릭해주세요</Button>
+        <Button full={true} disabled={true}>
+          클릭해주세요
+        </Button>
+      </div>
     </div>
   )
 }
--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -127,4 +127,16 @@ export default css`
     border-collapse: collapse;
     border-spacing: 0;
   }
+
+  button {
+    border: none;
+    margin: 0;
+    padding: 0;
+    width: auto;
+    overflow: visible;
+    background: transparent;
+    color: inherit;
+    font: inherit;
+    line-height: normal;
+  }
 `
