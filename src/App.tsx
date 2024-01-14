ddf927776652bda9ce5a69a6ae445e1d0adc5763
yewon97 <dpdnjs9936@gmail.com>
2024-01-14 23:56:47 +0900

    app.tsx에 불필요한 내용 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,4 @@
-import './App.css'
-import Text from '@shared/Text'
-import Button from '@shared/Button'
-import Input from '@components/shared/Input'
-import TextField from '@components/shared/TextField'
-import Alert from './components/shared/Alert'
-
-import { useAlertContext } from '@contexts/AlertContext'
-
 function App() {
-  const { open } = useAlertContext()
-  console.log('open: ', open)
-
   return <div></div>
 }
 
