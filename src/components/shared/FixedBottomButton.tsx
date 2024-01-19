72835aea35698afe3ff5c8d50f840f756361d0a2
yewon97 <dpdnjs9936@gmail.com>
2024-01-19 23:15:09 +0900

    fixed bottom 버튼에 애니메이션 추가

--- src/components/shared/FixedBottomButton.tsx
+++ src/components/shared/FixedBottomButton.tsx
@@ -1,5 +1,5 @@
 import Button from '@shared/Button'
-import { css } from '@emotion/react'
+import { css, keyframes } from '@emotion/react'
 import styled from '@emotion/styled'
 import { createPortal } from 'react-dom'
 import { colors } from '@styles/colorPalette'
@@ -29,13 +29,29 @@ export default function FixedBottomButton({
   )
 }
 
+const slideup = keyframes`
+	to {
+		transform: translateY(0);
+	}
+`
+
 const Container = styled.div`
   position: fixed;
   left: 0;
   right: 0;
   bottom: 0;
-  background-color: ${colors.white};
+  background: rgb(255, 255, 255);
+  background: linear-gradient(
+    360deg,
+    rgba(255, 255, 255, 1) 0%,
+    rgba(255, 255, 255, 0.955641631652661) 25%,
+    rgba(255, 255, 255, 0.894016981792717) 50%,
+    rgba(255, 255, 255, 0.5662858893557423) 75%,
+    rgba(255, 255, 255, 0.22735031512605042) 100%
+  );
   padding: 20px 10px 8px;
+  transform: translateY(100%);
+  animation: ${slideup} 0.5s ease-in-out forwards;
 `
 const buttonStyles = css`
   border-radius: 8px;
