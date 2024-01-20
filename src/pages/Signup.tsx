b08a5aadd9470d7c5d25b23c17d2984a915b3912
yewon97 <dpdnjs9936@gmail.com>
2024-01-21 01:21:57 +0900

    회원가입 처리하기

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,4 +1,5 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '@remote/firebase'
 
 import Form from '@signup/Form'
 import { FormValues } from '@models/signup'
@@ -6,8 +7,12 @@ import { FormValues } from '@models/signup'
 export default function SignupPage() {
   // Form에 안만들고 page 안 signup 페이지에 회원가입 로직을 만듬
 
-  const handleSubmit = (formValues: FormValues) => {
+  const handleSubmit = async (formValues: FormValues) => {
     console.log('formValues: ', formValues)
+    const { email, password } = formValues
+
+    const user = await createUserWithEmailAndPassword(auth, email, password)
+    console.log('user: ', user)
   }
 
   return (
