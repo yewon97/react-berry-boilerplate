9c670eb5f21dc33b87ee11876ba9a80f9799d718
yewon97 <dpdnjs9936@gmail.com>
2024-01-22 23:14:28 +0900

    user 등록후 newUser생성하기

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,5 +1,6 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
 import { auth } from '@remote/firebase'
+import { collection, doc, setDoc } from 'firebase/firestore'
 
 import Form from '@signup/Form'
 import { FormValues } from '@models/signup'
@@ -8,11 +9,19 @@ export default function SignupPage() {
   // Form에 안만들고 page 안 signup 페이지에 회원가입 로직을 만듬
 
   const handleSubmit = async (formValues: FormValues) => {
-    console.log('formValues: ', formValues)
-    const { email, password } = formValues
+    const { email, password, name } = formValues
 
-    const user = await createUserWithEmailAndPassword(auth, email, password)
-    console.log('user: ', user)
+    const { user } = await createUserWithEmailAndPassword(auth, email, password)
+
+    await updateProfile(user, {
+      displayName: name,
+    })
+
+    const newUser = {
+      uid: user.uid,
+      email: user.email,
+      displayName: name,
+    }
   }
 
   return (
