981e0a3e0a4fc26ab386edab0312dacdb524ed02
yewon97 <dpdnjs9936@gmail.com>
2024-01-24 23:50:22 +0900

    로그인 처리하고 응답값 콘솔 찍어보기

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -2,8 +2,16 @@ import { FormValues } from '@models/signin'
 import Form from '@components/signin/Form'
 import { useCallback } from 'react'
 
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '@remote/firebase'
+
 export default function SigninPage() {
-  const handleSubmit = useCallback((formValues: FormValues) => {}, [])
+  const handleSubmit = useCallback(async (formValues: FormValues) => {
+    const { email, password } = formValues
+
+    const response = await signInWithEmailAndPassword(auth, email, password)
+    console.log('response: ', response)
+  }, [])
 
   return (
     <form>
