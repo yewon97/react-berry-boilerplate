99e0d73cf983fca0d5207088c0eb6c66d4979a3f
yewon97 <dpdnjs9936@gmail.com>
2024-01-24 23:59:07 +0900

    로그인 실패시 알럿 띄우기

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -4,14 +4,45 @@ import { useCallback } from 'react'
 
 import { signInWithEmailAndPassword } from 'firebase/auth'
 import { auth } from '@remote/firebase'
+import { useAlertContext } from '@contexts/AlertContext'
+import { FirebaseError } from 'firebase/app'
 
 export default function SigninPage() {
-  const handleSubmit = useCallback(async (formValues: FormValues) => {
-    const { email, password } = formValues
+  const { open } = useAlertContext()
 
-    const response = await signInWithEmailAndPassword(auth, email, password)
-    console.log('response: ', response)
-  }, [])
+  const handleSubmit = useCallback(
+    async (formValues: FormValues) => {
+      const { email, password } = formValues
+
+      try {
+        const response = await signInWithEmailAndPassword(auth, email, password)
+        console.log('response: ', response)
+      } catch (e) {
+        // firebase의 에러
+        if (e instanceof FirebaseError) {
+          if (e.code === 'auth/wrong-password') {
+            open({
+              title: '계정의 정보를 다시 확인해주세요',
+              onButtonClick: () => {
+                //
+              },
+            })
+
+            return
+          }
+        }
+
+        // 일반적인 에러
+        open({
+          title: '잠시 후 다시 시도해주세요',
+          onButtonClick: () => {
+            //
+          },
+        })
+      }
+    },
+    [open],
+  )
 
   return (
     <form>
