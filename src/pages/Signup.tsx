83c16b63468e3aa2bd54ad3a6fb00c1ea06ab34c
yewon97 <dpdnjs9936@gmail.com>
2024-01-28 22:43:46 +0900

    회원가입 성공시 홈으로 이동

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -6,8 +6,12 @@ import Form from '@components/signup/Form'
 import { FormValues } from '@models/signup'
 import { COLLECTIONS } from '@constants'
 
+import { useNavigate } from 'react-router-dom'
+
 export default function SignupPage() {
   // Form에 안만들고 page 안 signup 페이지에 회원가입 로직을 만듬
+
+  const navigate = useNavigate()
 
   const handleSubmit = async (formValues: FormValues) => {
     const { email, password, name } = formValues
@@ -25,6 +29,8 @@ export default function SignupPage() {
     }
 
     await setDoc(doc(collection(store, COLLECTIONS.USER), user.uid), newUser)
+
+    navigate('/')
   }
 
   return (
