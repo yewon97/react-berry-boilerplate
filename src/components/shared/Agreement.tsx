07094197d36fc729c33daabc268f8515ed3c2ac2
yewon97 <dpdnjs9936@gmail.com>
2024-01-29 22:17:06 +0900

    약관 동의 합성 컴포넌트 생성

---
+++ src/components/shared/Agreement.tsx
@@ -0,0 +1,24 @@
+import { css } from '@emotion/react'
+import Flex from '@components/shared/Flex'
+import Text from '@components/shared/Text'
+
+export default function Agreement() {
+  return <div></div>
+}
+function AgreementTitle() {
+  return <div></div>
+}
+function AgreementDescription() {
+  return <div></div>
+}
+
+Agreement.Title = AgreementTitle
+Agreement.Description = AgreementDescription
+
+/**
+ * <Agreement>
+ * 	<Agreement.Title></Agreement.Title>
+ * 	<Agreement.Description></Agreement.Description>
+ * 	<Agreement.Description></Agreement.Description>
+ * </Agreement>
+ */
