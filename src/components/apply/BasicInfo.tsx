e0b8ea585c76d8ab9148a2a4b42e2fb7876e9ce7
yewon97 <dpdnjs9936@gmail.com>
2024-02-07 00:20:45 +0900

    select value 값 설정하기

--- src/components/apply/BasicInfo.tsx
+++ src/components/apply/BasicInfo.tsx
@@ -19,19 +19,19 @@ export default function BasicInfo() {
         label="연소득"
         options={연소득옵션}
         placeholder={연소득옵션[0].label}
-        value=""
+        value={infoValues.salary}
       />
       <Select
         label="신용점수"
         options={신용점수옵션}
         placeholder={신용점수옵션[0].label}
-        value=""
+        value={infoValues.creditScore}
       />
       <Select
         label="결제일"
         options={결제일옵션}
         placeholder={결제일옵션[0].label}
-        value=""
+        value={infoValues.payDate}
       />
     </div>
   )
