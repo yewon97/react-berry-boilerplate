ff4d7eb9acdc0c12177ec07c1397ea135916447e
yewon97 <dpdnjs9936@gmail.com>
2024-02-12 01:05:56 +0900

    button의 상태변화를 data-set 프로퍼티를 이용해서 체크할 것임

--- src/components/apply/CardInfo.tsx
+++ src/components/apply/CardInfo.tsx
@@ -1,7 +1,7 @@
 import Button from '@shared/Button'
 import Spacing from '@shared/Spacing'
 import { ApplyValues } from '@models/apply'
-import { useState } from 'react'
+import { MouseEvent, useCallback, useState } from 'react'
 
 type CardInfoValues = Pick<ApplyValues, 'isMaster' | 'isHipass' | 'isRf'>
 
@@ -14,13 +14,28 @@ export default function CardInfo() {
 
   const { isHipass, isMaster, isRf } = cardInfoValues
 
+  const handleButtonClick = useCallback((e: MouseEvent<HTMLButtonElement>) => {
+    const $button = e.target as HTMLButtonElement
+    console.log('$button: ', $button.dataset)
+  }, [])
+
   return (
     <div>
       <Button.Group title="해외결제">
-        <Button weak={isMaster === false} size="medium">
+        <Button
+          weak={isMaster === false}
+          size="medium"
+          data-value={true}
+          onClick={handleButtonClick}
+        >
           Master
         </Button>
-        <Button weak={isMaster === true} size="medium">
+        <Button
+          weak={isMaster === true}
+          size="medium"
+          data-value={false}
+          onClick={handleButtonClick}
+        >
           국내전용
         </Button>
       </Button.Group>
@@ -28,10 +43,20 @@ export default function CardInfo() {
       <Spacing size={12} />
 
       <Button.Group title="후불교통카드">
-        <Button weak={isRf === true} size="medium">
+        <Button
+          weak={isRf === true}
+          size="medium"
+          data-value={false}
+          onClick={handleButtonClick}
+        >
           신청안함
         </Button>
-        <Button weak={isRf === false} size="medium">
+        <Button
+          weak={isRf === false}
+          size="medium"
+          data-value={true}
+          onClick={handleButtonClick}
+        >
           신청
         </Button>
       </Button.Group>
@@ -39,10 +64,20 @@ export default function CardInfo() {
       <Spacing size={12} />
 
       <Button.Group title="후불하이패스카드">
-        <Button weak={isHipass === true} size="medium">
+        <Button
+          weak={isHipass === true}
+          size="medium"
+          data-value={false}
+          onClick={handleButtonClick}
+        >
           신청안함
         </Button>
-        <Button weak={isHipass === false} size="medium">
+        <Button
+          weak={isHipass === false}
+          size="medium"
+          data-value={true}
+          onClick={handleButtonClick}
+        >
           신청
         </Button>
       </Button.Group>
