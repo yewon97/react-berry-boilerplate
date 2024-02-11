afdde5d09c9eed273c34913b0b33ab4bad1773d8
yewon97 <dpdnjs9936@gmail.com>
2024-02-12 01:01:42 +0900

    type에 cardinfo 정보 추가, 상태에 따른 버튼 선택한지 ui로 보여주기

--- src/components/apply/CardInfo.tsx
+++ src/components/apply/CardInfo.tsx
@@ -1,19 +1,50 @@
 import Button from '@shared/Button'
+import Spacing from '@shared/Spacing'
+import { ApplyValues } from '@models/apply'
+import { useState } from 'react'
+
+type CardInfoValues = Pick<ApplyValues, 'isMaster' | 'isHipass' | 'isRf'>
 
 export default function CardInfo() {
+  const [cardInfoValues, setCardInfoValues] = useState<CardInfoValues>({
+    isMaster: false,
+    isHipass: false,
+    isRf: false,
+  })
+
+  const { isHipass, isMaster, isRf } = cardInfoValues
+
   return (
     <div>
       <Button.Group title="해외결제">
-        <Button size="medium">Master</Button>
-        <Button size="medium">국내전용</Button>
+        <Button weak={isMaster === false} size="medium">
+          Master
+        </Button>
+        <Button weak={isMaster === true} size="medium">
+          국내전용
+        </Button>
       </Button.Group>
+
+      <Spacing size={12} />
+
       <Button.Group title="후불교통카드">
-        <Button size="medium">신청안함</Button>
-        <Button size="medium">신청</Button>
+        <Button weak={isRf === true} size="medium">
+          신청안함
+        </Button>
+        <Button weak={isRf === false} size="medium">
+          신청
+        </Button>
       </Button.Group>
+
+      <Spacing size={12} />
+
       <Button.Group title="후불하이패스카드">
-        <Button size="medium">신청안함</Button>
-        <Button size="medium">신청</Button>
+        <Button weak={isHipass === true} size="medium">
+          신청안함
+        </Button>
+        <Button weak={isHipass === false} size="medium">
+          신청
+        </Button>
       </Button.Group>
     </div>
   )
