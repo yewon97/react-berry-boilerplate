aa0f75bf39f09d44ae2f9fed5125d1e642dcf0c6
yewon97 <dpdnjs9936@gmail.com>
2024-02-18 20:21:45 +0900

    신청 상태값 폴링으로 구현

--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -1,11 +1,25 @@
 import { useState } from 'react'
 import Apply from '@/components/apply'
 import useApplyCardMutation from '@components/apply/hooks/useApplyCardMutation'
+import usePollApplyStatus from '@components/apply/hooks/usePollApplyStatus'
 
 export default function ApplyPage() {
+  const [readyToPoll, setReadyToPoll] = useState(false)
+
+  usePollApplyStatus({
+    onSuccess: () => {
+      console.log('성공')
+    },
+    onError: () => {
+      console.log('실패')
+    },
+    enabled: readyToPoll,
+  })
+
   const { mutate } = useApplyCardMutation({
     onSuccess: () => {
       // 값이 추가되었을 때 => 폴링시작
+      setReadyToPoll(true)
     },
     onError: () => {
       // 실패했을 때 => 폴링시작
