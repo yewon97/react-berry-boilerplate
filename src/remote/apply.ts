7c38a79cae44212ab26f0b29e0d1d5913dbe9f51
yewon97 <dpdnjs9936@gmail.com>
2024-02-23 20:55:06 +0900

    유저가 가지고 있는 카드 반환해주는 함수 생성

--- src/remote/apply.ts
+++ src/remote/apply.ts
@@ -8,7 +8,7 @@ import {
   updateDoc,
   where,
 } from 'firebase/firestore'
-import { store } from './firebase'
+import { app, store } from './firebase'
 
 export async function applyCard(applyValues: ApplyValues) {
   return addDoc(collection(store, COLLECTIONS.CARD_APPLY), applyValues)
@@ -34,4 +34,29 @@ export async function updateApplyCard({
   const [applied] = snapshot.docs
 
   updateDoc(applied.ref, applyValues)
+}
+
+// 유저가 가지고 있는 카드 반환해주는 함수
+export async function getAppliedCard({
+  userId,
+  cardId,
+}: {
+  userId: string
+  cardId: string
+}) {
+  const snapshot = await getDocs(
+    query(
+      collection(store, COLLECTIONS.CARD_APPLY),
+      where('userId', '==', userId),
+      where('cardId', '==', cardId),
+    ),
+  )
+
+  if (snapshot.docs.length === 0) {
+    return null
+  }
+
+  const [applied] = snapshot.docs
+
+  return applied.data() as ApplyValues
 }
