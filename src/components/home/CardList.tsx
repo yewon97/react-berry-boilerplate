982d80445aec41782937a9a4607da834125b0fc3
yewon97 <dpdnjs9936@gmail.com>
2024-03-03 20:06:41 +0900

    lodash.flatten 사용 후 번들사이즈 확인하기

--- src/components/home/CardList.tsx
+++ src/components/home/CardList.tsx
@@ -3,7 +3,7 @@ import { getCards } from '@remote/card'
 import ListRow from '@shared/ListRow'
 import { Card, CardWithId } from '@models/card'
 
-import { flatten } from 'lodash'
+import flatten from 'lodash.flatten'
 import { useCallback } from 'react'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import Badge from '@shared/Badge'
